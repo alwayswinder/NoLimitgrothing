22886ccbc9023e3bb02b3d40077428d905560d56
alwayswinder <1181612350@qq.com>
2023-02-12 16:51:12 +0800

    first commit

---
+++ Source/NoLimitGrowing/NoLimitGrowingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NoLimitGrowingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOLIMITGROWING_API ANoLimitGrowingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
