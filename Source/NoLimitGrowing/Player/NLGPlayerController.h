382ab6e178293e8b687db024738c0312abbeba6c
侯富林 <fulin.hou@yingxiong.com>
2023-02-13 14:26:40 +0800

    base c++ class

---
+++ Source/NoLimitGrowing/Player/NLGPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NLGPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOLIMITGROWING_API ANLGPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
